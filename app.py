bcf687af099e96992a0d08a4d73198a16c6fa450
zakie khan <121384625+zakie2003@users.noreply.github.com>
2024-09-17 18:15:16 +0530

    Cosine Similarity Added

--- app.py
+++ app.py
@@ -8,64 +8,45 @@ app=Flask(__name__)
 def index():
     if(request.method=='POST'):
 
-        age=request.form["age"]
-        gender=request.form["gender"]
-        learning_style=request.form["learning_style"]
-        previous_score=request.form["previous_score"]
-        subject=request.form["subject"]
-        difficulty=request.form["difficulty"]
-        question_type=request.form["question_type"]
-        # correct=request.form["correct"]
-        time_taken=request.form["time_taken"]
-        accuracy=request.form["accuracy"]
+        TOC=request.form["TOC"]
+        Data_Science=request.form["Data Science"]
+        Average_Time_Taken=request.form["Average Time Taken"]
+        prefence=request.form["Preference of Learning"]
+        score=request.form["Previous Score"]
+        accuracy=request.form["Accuracy of Student"]
 
         # Checking if all the fields are filled
-        if(not all ([age,gender,learning_style,previous_score,subject,difficulty,question_type,time_taken,accuracy])):
+        if(not all ([TOC,Data_Science,Average_Time_Taken,prefence,score,accuracy])):
             return make_response("all fields are required",400)
         
         temp_data={
-            "Age": [age],
-            "Gender": [gender],
-            "Learning_Style": [learning_style],
-            "Previous_Score": [previous_score],
-            "Subject": [subject],
-            "Difficulty": [difficulty],
-            "Question_Type": [question_type],
-            # "Correct": [correct],
-            "Time_Taken": [time_taken],
-            "Accuracy": [accuracy]
+            "TOC": [TOC],
+            "Data Science": [Data_Science],
+            "Average Time Taken": [Average_Time_Taken],
+            "Preference of Learning": [prefence],
+            "Previous Score": [score],
+            "Accuracy of Student": [accuracy],
         }
 
         student_profile = pd.DataFrame(temp_data)
         
-        question_data=pd.read_csv("./DataSet/student_api.csv")
+        question_data=pd.read_csv("./DataSet/Question_Set.csv")
 
+        question_data["Question Difficulty"]=question_data["Question Difficulty"].map({"Easy":1,"Medium":2,"Hard":3})
 
+        # print(question_data)
+        question_data.columns=["TOC","Data Science","Average Time Taken","Preference of Learning","Previous Score","Accuracy of Student"]
         all_data=pd.concat([question_data,student_profile],axis=0)
 
-
-
         #Scaling numarical Values
+
         scaler=MinMaxScaler()
-
-        all_data[["Age","Previous_Score","Time_Taken","Accuracy"]]=scaler.fit_transform(all_data[["Age","Previous_Score","Time_Taken","Accuracy"]])
-
-        #Encoding Categorical Values
-        all_data["Difficulty"]=all_data["Difficulty"].map({"Easy":1,"Medium":2,"Hard":3})
-        all_data[all_data["Gender"].unique()]=pd.get_dummies(all_data["Gender"])*1
-        all_data[all_data["Learning_Style"].unique()]=pd.get_dummies(all_data["Learning_Style"])*1
-        all_data[all_data["Subject"].unique()]=pd.get_dummies(all_data["Subject"])*1
-
-        
-        #Calculating cosine similarity
-        all_data=all_data.drop(["Gender","Learning_Style","Correct","Subject","Question_Type"],axis=1)
-
-
-        print(all_data)
+        all_data[["Average Time Taken"]]=scaler.fit_transform(all_data[["Average Time Taken"]])
+        all_data[all_data["Preference of Learning"].unique()]=pd.get_dummies(all_data["Preference of Learning"])*1
+        all_data=all_data.drop(columns=["Preference of Learning"])
 
         similarity=cosine_similarity(all_data.iloc[-1].values.reshape(1,-1),all_data.iloc[:-1].values)
-
-
+        
         cosine_sim_df = pd.DataFrame({"Cosine similarity":similarity[0]})
         print(cosine_sim_df)
         top_similar_students = cosine_sim_df["Cosine similarity"].sort_values(ascending=False).index[1:]
